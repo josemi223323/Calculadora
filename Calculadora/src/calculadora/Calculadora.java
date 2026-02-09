334093c1e75b8ff1565e4b4aaa9a0ee2c075e0b6
josemi <jrubioaleman@gmail.com>
2026-02-09 12:37:28 +0100

    añadir

---
+++ Calculadora/src/calculadora/Calculadora.java
@@ -0,0 +1,60 @@
+package calculadora;
+
+import java.util.ArrayList;
+
+public class Calculadora {
+	private static LeerInt lectorInteger = new LeerInt();
+	private int pilaNumeros;
+	private int resultadoAnterior;
+	private ArrayList<Integer>resultados = new ArrayList<>();
+	public void sumar(int numerosASumar) {
+		for(int i=0;i<numerosASumar;i++) {
+			pilaNumeros += lectorInteger.lector("introduce un numero a sumar",true);
+		}
+		System.out.println("el resultado es " + pilaNumeros);
+		resultados.add(pilaNumeros);
+		pilaNumeros = 0;
+	}
+	public void restar(int numerosARestar) {
+		for(int i=0;i<numerosARestar;i++) {
+			if(i==0) {
+				pilaNumeros = lectorInteger.lector("introduce un numero a restar",true);
+			}else {
+				pilaNumeros -= lectorInteger.lector("introduce un numero a restar",true);
+			}
+		}
+		System.out.println("el resultado es " + pilaNumeros);
+		resultados.add(pilaNumeros);
+		pilaNumeros = 0;
+	}
+	public void multiplicar(int numerosAMultiplicar) {
+		for(int i=0;i<numerosAMultiplicar;i++) {
+			if(i==0) {
+				pilaNumeros = lectorInteger.lector("introduce un numero a multiplicar",true);
+			}else {
+				pilaNumeros *= lectorInteger.lector("introduce un numero a multiplicar",true);
+			}
+		}
+		System.out.println("el resultado es " + pilaNumeros);
+		resultados.add(pilaNumeros);
+		pilaNumeros = 0;
+	}
+	public void dividir(int numerosADividir) {
+		for(int i=0;i<numerosADividir;i++) {
+			if(i==0) {
+				pilaNumeros = lectorInteger.lector("introduce un numero a multiplicar",true);
+			}else {
+				pilaNumeros /= lectorInteger.lector("introduce un numero a multiplicar",true);
+			}
+		}
+		System.out.println("el resultado es " + pilaNumeros);
+		resultados.add(pilaNumeros);
+		pilaNumeros = 0;
+	}
+	public int getResultadoAnterior() {
+		resultadoAnterior = resultados.get(resultados.size()-1);
+		return resultadoAnterior;
+	}
+	
+	
+}
